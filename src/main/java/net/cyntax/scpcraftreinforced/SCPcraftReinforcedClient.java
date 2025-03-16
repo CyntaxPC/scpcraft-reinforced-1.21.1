5e4ea545655fe8e8f3d257f0d3b6bbfc6eb0a5e2
Cyntax <matthewdlibby@gmail.com>
2025-03-16 13:13:46 -0400

    Initial commit

---
+++ src/main/java/net/cyntax/scpcraftreinforced/SCPcraftReinforcedClient.java
@@ -0,0 +1,10 @@
+package net.cyntax.scpcraftreinforced;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SCPcraftReinforcedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/cyntax/scpcraftreinforced/SCPcraftReinforcedDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cyntax.scpcraftreinforced;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SCPcraftReinforcedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
